67c0891d470a64f6ff8b41819e6031bf1e6ba867
Eric Tao <erictao04@gmail.com>
2023-06-14 22:50:30 -0400

    checks if stock exists

--- holding.cpp
+++ holding.cpp
@@ -14,8 +14,6 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 
 
 double Holding::getSharePrice() {
-    // TODO verify ticker exists
-
     CURL *curl;
     CURLcode res;
     std::string response;
@@ -65,6 +63,31 @@ int Holding::getNumShares() {
 }
 
 bool Holding::exists() {
-    // TODO check if ticker exists
-    return true;
+    CURL *curl;
+    CURLcode res;
+    std::string response;
+    struct curl_slist* headers = NULL;
+    headers = curl_slist_append(headers, "Accept: application/json");
+    headers = curl_slist_append(headers, "Content-Type: application/json"); 
+
+    curl = curl_easy_init();
+    if(curl) {
+        string url = "https://query1.finance.yahoo.com/v11/finance/quoteSummary/" + ticker + "?modules=financialData";
+        
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        Json::Reader reader;
+        Json::Value completeJsonData;
+
+        reader.parse(response, completeJsonData);
+        completeJsonData["quoteSummary"]["error"];
+
+        return !completeJsonData["quoteSummary"].get("error", false);
+    }
+
+    return false;
 }
